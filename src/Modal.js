1d8fb61197de677f09e25f367ae4698ba8b81579
Athul Rajagopal <49791750+rajagoat@users.noreply.github.com>
2021-05-18 02:17:59 -0600

    Genre tags properly show up

--- src/Modal.js
+++ src/Modal.js
@@ -3,14 +3,15 @@ import NA from './img/NA.svg';
 import { motion, AnimatePresence } from 'framer-motion';
 import useFetch from './useFetch';
 import exitIcon from './img/exit-icon.svg';
+import { useEffect } from 'react';
 
 const backdrop = {
     visible: { opacity: 1 },
     hidden: { opacity: 0 }
 };
 
-const Modal = ({ showModal, setShowModal, id }) => {
-    const API = `http://www.omdbapi.com/?i=${id}&apikey=da1f5ac0&plot=full`;
+const Modal = ({ showModal, setShowModal, id, setId }) => {
+    const API = `http://www.omdbapi.com/?i=${id}&apikey=da1f5ac0&plot=short`;
     // console.log(API);
     const { data, isPending } = useFetch(API);
     /* if (data != null) {
@@ -19,10 +20,49 @@ const Modal = ({ showModal, setShowModal, id }) => {
 
     if (data != null) {
         const img = document.querySelector('.poster');
-        if (img.getAttribute('src') === 'N/A') {
-            img.setAttribute('src', NA);
+        if (img !== null) {
+            if (img.getAttribute('src') === 'N/A') {
+                img.setAttribute('src', NA);
+            }
         }
     }
+
+    useEffect(() => {
+        const content = document.querySelector('.title-modal');
+        if (data !== null) {
+            var genre = data.Genre; // this has global scope
+        }
+        const genreTags = [
+            'Action',
+            'Adventure',
+            'Animation',
+            'Biography',
+            'Comedy',
+            'Crime',
+            'Documentary',
+            'Drama',
+            'Family',
+            'Fantasy',
+            'Horror',
+            'Music',
+            'Mystery',
+            'Romance',
+            'Sci-Fi',
+            'Short',
+            'Sport',
+            'Thriller',
+            'War'
+        ]
+        // console.log(genre);
+        if (genre !== undefined && content !== null) {
+            genreTags.forEach(tag => {
+                if (genre.includes(tag)) {
+                    // console.log(`<p>${tag}</p>`);
+                    content.insertAdjacentHTML('afterend', `<p>${tag}</p>`);
+                }
+            });
+        }
+    }, [data]);
 
     return (
         <AnimatePresence exitBeforeEnter>
@@ -36,10 +76,12 @@ const Modal = ({ showModal, setShowModal, id }) => {
                     {data && <motion.div className="modal">
                         <img className="poster" src={data.Poster} alt="Movie Poster" />
                         <div className="data">
-                            <img src={exitIcon} alt="Exit Icon" onClick={() => setShowModal(false)} />
+                            <img src={exitIcon} alt="Exit Icon" onClick={() => {
+                                setShowModal(false)
+                                setId('');
+                            }} />
                             <div className="info">
-                                <h3>{data.Title}</h3>
-                                {/* <p>{getGenre(data.Genre)}</p> */}
+                                <h3 className="title-modal">{data.Title}</h3>
                                 <p><b>Actors: </b>{data.Actors}</p>
                                 <p className="plot"><b>Plot: </b>{data.Plot}</p>
                             </div>
