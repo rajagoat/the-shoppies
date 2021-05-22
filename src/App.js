0ccf9bbf73cf92369c22645082366df6023ccd28
Athul Rajagopal <49791750+rajagoat@users.noreply.github.com>
2021-05-21 19:33:58 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,10 +15,10 @@ function App() {
       <div className="App">
         <Navbar />
         <Switch>
-          <Route exact path='/#/'>
+          <Route exact path='/'>
             <Home nomData={nomData}/>
           </Route>
-          <Route path='/#/nominations'>
+          <Route path='/nominations'>
             <Nominations nomData={nomData}/>
           </Route>
         </Switch>
